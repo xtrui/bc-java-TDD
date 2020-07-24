6ebbbba89b3c2d4417009a7e08d58974d5d6ef84
Ray <ray.wen@oocl.com>
2020-07-24 09:14:18 +0800

    [FizzBuzzWhizz][Ray] refactor: refactor play method.

--- src/main/java/junit5/tdd/FiZZBuzzWhizz.java
+++ src/main/java/junit5/tdd/FiZZBuzzWhizz.java
@@ -9,15 +9,24 @@ public class FiZZBuzzWhizz {
         String result = "";
         if (count == 1) {
             result = "1";
+            System.out.println(result);
+
         } else {
-            if (count % 3 == 0) {
-                result += "Fizz";
-            }
-            if (count % 5 == 0) {
-                result += "Buzz";
-            }
-            if (count % 7 == 0) {
-                result += "Whizz";
+            for (int i = 1; i <= count; i++) {
+                result = "";
+                if (i % 3 == 0) {
+                    result += "Fizz";
+                }
+                if (i % 5 == 0) {
+                    result += "Buzz";
+                }
+                if (i % 7 == 0) {
+                    result += "Whizz";
+                }
+                if (result == "") {
+                    result = String.valueOf(i);
+                }
+                System.out.println(result);
             }
         }
 
